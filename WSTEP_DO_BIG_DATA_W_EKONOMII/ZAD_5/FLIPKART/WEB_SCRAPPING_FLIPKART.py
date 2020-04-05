0472ade1dc470c8b39e6e0e6a4c4a910976b9505
Sławomir Kranc <skranc@edu.cdv.pl>
2020-04-05 22:02:48 +0200

    05.04.2020
    05.04.2020

---
+++ WSTEP_DO_BIG_DATA_W_EKONOMII/ZAD_5/FLIPKART/WEB_SCRAPPING_FLIPKART.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Autor: Sławomir Kranc
+
+This is a base script file.
+"""
+
+from selenium import webdriver
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+driver = webdriver.Chrome("D:/chromedriver.exe")
+
+
+products=[] #List to store name of the product
+prices=[] #List to store price of the product
+ratings=[] #List to store rating of the product
+driver.get("https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+
+
+content = driver.page_source
+soup = BeautifulSoup(content, "lxml")
+
+
+def main():
+
+    for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
+        name=a.find('div', attrs={'class':'_3wU53n'})
+        price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
+        rating=a.find('div', attrs={'class':'hGSR34'})
+    
+        products.append(name.text)
+        prices.append(price.text)
+        ratings.append(rating.text) 
+    
+       
+    df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings})
+    df.to_csv('D:/products.csv',index = False)
+    print(df)
+
+if __name__ == "__main__":
+  main()
---
+++ WSTEP_DO_BIG_DATA_W_EKONOMII/ZAD_5/GIEŁDA/WEB_SCRAPPING_GIEŁDA.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Autor: Sławomir Kranc
+
+This is a base script file.
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    
+    while True:
+
+        company = input("Please type company ID. Example ccc or pkn or wig20\t:")
+        print('You picked {} company'.format(company))
+        
+        
+        _id = {"s": company}
+        website = requests.get("http://stooq.pl/q/", params=_id)
+        soup = BeautifulSoup(website.text, 'lxml')
+        
+        
+        #Find current rate
+        find_rate = soup.find("span", id=f"aq_{company}_c2")
+        rate = float(find_rate.text)
+        print('Current rate for {} company is: {} zł'.format(company, rate))
+        
+        
+        #Find company absolute change in percents[%]
+        find_absolute_change = soup.find("span", id=f"aq_{company}_m2")
+        absolute_change= float(find_absolute_change.text)
+        print('Current absolute change for {} company is: {} %'.format(company, absolute_change))
+        
+        
+        #Find company relative change in percents[%]
+        find_relative_change = soup.find("span", id=f"aq_{company}_m3")
+        relative_change = float(find_relative_change.text[1:-2])/100
+        print('Current relative change for {} company is: {} %'.format(company, relative_change))
+        
+        
+        #Find company transaction count
+        transaction_count = soup.find(text="Transakcje").next_element.next_element
+        transactions = int(transaction_count.text.replace(" ", ""))
+        print('Current ransaction count for {} company is: {} '.format(company, transactions))
+        
+        while True:
+          decision = input('Do you want check another company? (t/n) ')
+          if decision == 't':
+            break
+          elif decision == 'n':
+            print('Bye!')
+            input('Type any key to close program ...')
+            return
+          else:
+            print('Please type correct answer! (t/n)')
+
+if __name__ == "__main__":
+  main()
